1c63820b48ea04e74fd30fb432a9318fa0b29a00
Mohamed Elsayed <moelsayed524@gmail.com>
2026-01-15 22:36:46 +0200

    final touch

--- src/pages/home/HomeSticky.tsx
+++ src/pages/home/HomeSticky.tsx
@@ -259,7 +259,7 @@ const HomeSticky = () => {
         className="absolute max-lg:z-10"
         style={{
           left: `${isLg ? borderWidth - 2 : 4}px`,
-          top: `${isLg ? borderWidth - 2 : 66}px`,
+          top: `${isLg ? borderWidth - 2 : 66.3}px`,
           width: `${isLg ? borderWidth : borderWidth + 24}px`,
           height: `${isLg ? borderWidth : borderWidth + 24}px`,
           transform: 'rotate(180deg)',
